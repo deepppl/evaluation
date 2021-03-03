5552e402a1f6da14a43cbb4a9fb729a57969d237
Louis Mandel <lmandel@us.ibm.com>
2021-03-03 14:36:02 -0500

    Update test_example-models.sh

--- rq1/test_example-models.sh
+++ rq1/test_example-models.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-function compile_all {
-    backend=$1
-    mode=$2
-    logfile=logs/$backend-$mode.csv
-    mkdir -p logs
-    echo --- $backend $mode
-    rm -f /tmp/out-$backend-$mode $logfile /tmp/err-$backend-$mode
-    find ../example-models -name *.stan -exec bash ./compile.sh $backend $mode $logfile {} \;
-    echo
-    echo Success:        `cat $logfile | grep ", 0" | wc -l`
-    echo stanc failures: `cat $logfile | grep ", 1" | wc -l`
-    echo stanc-$backend-$mode failures:  `cat $logfile | grep ", 2" | wc -l`
-    echo Total:          `cat $logfile | wc -l`
-}
-compile_all pyro generative
-compile_all pyro comprehensive
-compile_all pyro mixed
-compile_all numpyro generative
-compile_all numpyro comprehensive
-compile_all numpyro mixed
+backend=$1
+mode=$2
+logfile=logs/$backend-$mode.csv
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 backend mode"
+    echo "  backend: pyro | numpyro"
+    echo "  mode:    comprehensive | mixed | generative"
+    exit 1
+fi
+
+mkdir -p logs
+echo --- $backend $mode
+rm -f /tmp/out-$backend-$mode $logfile /tmp/err-$backend-$mode
+find ../example-models -name *.stan -exec bash ./compile.sh $backend $mode $logfile {} \;
+echo
+echo Success:        `cat $logfile | grep ", 0" | wc -l`
+echo stanc failures: `cat $logfile | grep ", 1" | wc -l`
+echo stanc-$backend-$mode failures:  `cat $logfile | grep ", 2" | wc -l`
+echo Total:          `cat $logfile | wc -l`
